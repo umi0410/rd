d2747165d9138acdd5dc5a517d6272e5d69ef046
Jinsu Park <dev.umijs@gmail.com>
2023-04-18 01:08:12 +0900

    frontend: Add title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Typography from '@mui/material/Typography';
 import AliasTable from './AliasTable'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <div className="App">
+      <Typography variant="h3" gutterBottom={true}>rd: redirecting by using aliases</Typography>
       <AliasTable group="default"></AliasTable>
     </div>
   )
